aa15d65d0514aaffaa7ddb864893b7cd33a785d2
StableDynamics <github@stabledynamics.net>
2025-03-05 09:51:06 +0000

    Gave descriptive summaries to methods

--- BankStatement.cpp
+++ BankStatement.cpp
@@ -20,21 +20,36 @@ BankStatement::~BankStatement(){
 }
 
 
+/**
+ * Prints a high level summary of the contents of the bank statement including
+ * bankName, monthly total income expenses arranged in years, and the averages
+ * from the period
+ */
 void BankStatement::printSummary(){
 
 }
 
 
+/**
+ * Determines the period that this accounting period is over based on the dates
+ * supplied
+ */
 void BankStatement::determineAccountingPeriod(){
 
 }
 
 
+/**
+ * Calculates the average monthly income
+ */
 void BankStatement::calculateAvgIncome(){
 
 }
 
 
+/**
+ * Calculates the average monthly expenditure
+ */
 void BankStatement::calculateAvgExpenditure(){
 
 }
